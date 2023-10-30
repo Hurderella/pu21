5bd73c8aef2ce23a82ddc3635ea88871bbc0145d
chansoon.heo <hurderella@gmail.com>
2023-10-30 14:46:10 +0900

    fix

--- matlab/examples/APPLY_METRIC.m
+++ matlab/examples/APPLY_METRIC.m
@@ -2,11 +2,11 @@
 hdrBasePath = "C:\\Users\\chan\\Documents\\HDR_DATASET\\CaveatsOfQA\\sihdr\\";
 reconPath = "reconstructions\\LFNet\\";
 referencePath = "reference\\";
-clipDelim = "clip_95\\";
-ext = "*.png";
+clipDelim = "LFNET_output_95\\";
+ext = ".hdr";
+refExt = ".exr";
 
-
-reconFullPath = hdrBasePath + reconPath + clipDelim + ext;
+reconFullPath = hdrBasePath + reconPath + clipDelim + "*" + ext;
 referenceFullPath = hdrBasePath + referencePath;
 
 reconFolderInfo = dir(reconFullPath);
@@ -15,21 +15,24 @@ for idx = 1:length(reconFolderInfo)
     
     reconFileName = reconFolderInfo(idx).name;
     reconFilePath = hdrBasePath + reconPath + clipDelim + reconFileName;
-    % baseFileName = baseFolderInfo(idx).name;
-    % baseFilePath = hdrBasePath + referencePath + clipDelim + baseFileName;
+    
+    referenceFileName = extractBefore(reconFileName, ext) + refExt;
+    referenceFilePath = hdrBasePath + referencePath + referenceFileName;
+
+
     fprintf("%s\n", reconFileName);
     fprintf("%s\n", reconFilePath);
-    fprintf("<<--->> %s \n", baseFileName);
-    fprintf("%s\n", baseFilePath);
+    fprintf("<<--->> %s \n", referenceFileName);
+    fprintf("%s\n", referenceFilePath);
 
     reconFile = hdrread(reconFilePath);
-    baseFile = hdrread(baseFilePath);
+    referenceFile = hdrread(referenceFilePath);
 
     Lpeak = 1000;
 
-    pu21_psnr = pu21_metric(reconFile, baseFile, 'PSNR');
-    pu21_fsim = pu21_metric(reconFile, baseFile, 'FSIM');
-    pu21_fsim_crf = pu21_metric(reconFile, baseFile, 'FSIM', 'crf_correction', true);
+    pu21_psnr = pu21_metric(reconFile, referenceFile, 'PSNR');
+    pu21_fsim = pu21_metric(reconFile, referenceFile, 'FSIM');
+    pu21_fsim_crf = pu21_metric(reconFile, referenceFile, 'FSIM', 'crf_correction', true);
     
     fprintf('PU21-PNSR : %g, PU21-FSIM : %g, PU21-FSIM-CRF : %g', ...
         pu21_psnr, pu21_fsim, pu21_fsim_crf);

6483f12ab550d5b1161fa3d674400b04207b936c
chansoon.heo <hurderella@gmail.com>
2023-10-30 21:51:16 +0900

    NLFNET q check (hdr, exr)

--- matlab/examples/APPLY_METRIC.m
+++ matlab/examples/APPLY_METRIC.m
@@ -2,7 +2,7 @@
 hdrBasePath = "C:\\Users\\chan\\Documents\\HDR_DATASET\\CaveatsOfQA\\sihdr\\";
 reconPath = "reconstructions\\LFNet\\";
 referencePath = "reference\\";
-clipDelim = "LFNET_output_95\\";
+clipDelim = "clip_95\\";
 ext = ".hdr";
 refExt = ".exr";
 
@@ -11,6 +11,9 @@ referenceFullPath = hdrBasePath + referencePath;
 
 reconFolderInfo = dir(reconFullPath);
 
+scoreTable = table('Size', [1, 5], ...
+    'VariableTypes', ["string", "double", "double", "double", "double"], ...
+    'VariableNames',["Name", "PU21-PSNR", "PU21-PIQE", "PU21-FSIM", "PU21-FSIM-CRF"]);
 for idx = 1:length(reconFolderInfo)
     
     reconFileName = reconFolderInfo(idx).name;
@@ -18,23 +21,33 @@ for idx = 1:length(reconFolderInfo)
     
     referenceFileName = extractBefore(reconFileName, ext) + refExt;
     referenceFilePath = hdrBasePath + referencePath + referenceFileName;
-
-
+    
+    pu21 = pu21_encoder();
+    
     fprintf("%s\n", reconFileName);
     fprintf("%s\n", reconFilePath);
     fprintf("<<--->> %s \n", referenceFileName);
     fprintf("%s\n", referenceFilePath);
 
     reconFile = hdrread(reconFilePath);
-    referenceFile = hdrread(referenceFilePath);
-
+    referenceFile = exrread(referenceFilePath);
+    
+    reconPiqeScore = piqe(pu21.encode(reconFile));
+    disp(reconPiqeScore);
+    
     Lpeak = 1000;
+    reconFile = reconFile / max(reconFile(:)) * Lpeak;
+    referenceFile = referenceFile / max(referenceFile(:)) * Lpeak;
 
     pu21_psnr = pu21_metric(reconFile, referenceFile, 'PSNR');
     pu21_fsim = pu21_metric(reconFile, referenceFile, 'FSIM');
     pu21_fsim_crf = pu21_metric(reconFile, referenceFile, 'FSIM', 'crf_correction', true);
-    
-    fprintf('PU21-PNSR : %g, PU21-FSIM : %g, PU21-FSIM-CRF : %g', ...
+
+    fprintf('PU21-PNSR : %g, PU21-FSIM : %g, PU21-FSIM-CRF : %g\n', ...
         pu21_psnr, pu21_fsim, pu21_fsim_crf);
-    break
+    fprintf("pu21 piqe recon : %g\n", reconPiqeScore);
+
+    curScore = {reconFileName, pu21_psnr, reconPiqeScore, pu21_fsim, pu21_fsim_crf};
+    scoreTable(idx, :) = curScore;
 end
+writetable(scoreTable, "NLFNet_clip_95.xls");
